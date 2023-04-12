a9d71d3a8827006beb14a137f28c9b60dc46fc42
Shobhit <shobhitmir@gmail.com>
2023-04-12 20:54:57 +0530

    deploying to render

--- deepmint/base_settings.py
+++ deepmint/base_settings.py
@@ -121,7 +121,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'nftgen', 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 CORS_ALLOW_ALL_ORIGINS = True
