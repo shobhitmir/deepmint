38abb48ad4975e1cb5b052acc92457f2b1aac1d6
Shobhit <shobhitmir@gmail.com>
2023-04-12 20:34:01 +0530

    deploying to render

--- deepmint/settings.py
+++ deepmint/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'nftgen', 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
